75d4f6c1eabd4477fc05a2db6e1dc3d61e9cb43b
xrkffgg <xrkffgg@vip.qq.com>
2020-12-07 13:34:19 +0800

    perf: add url

--- examples/App.vue
+++ examples/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <Layout title='zoo-issue-help'>
+  <Layout :title="title">
       <div>123</div>
   </Layout>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      title: {
+        name: 'Zoo Issue Helper',
+        url: 'https://github.com/zoo-js/zoo-issue-helper'
+      }
+    }
+  },
 }
 </script>
 
--- packages/Layout/Layout.jsx
+++ packages/Layout/Layout.jsx
@@ -2,38 +2,42 @@ import { defineComponent } from 'vue'
 import './Layout.scss'
 
 export default defineComponent({
-    name: 'Layout',
-    props: {
-        title: {
-            type: String,
-            default: () => {
-                return 'Zoo-Layout'
-            }
-        }
-    },
+	name: 'Layout',
+	props: {
+		title: {
+			type: Object,
+			default: () => {
+				return {
+					name: 'Zoo Layout',
+					url: 'https://github.com/zoo-js/zoo-layout',
+				}
+			}
+		}
+	},
 
-    setup(props, { slots }) {
-        const renderHeader = () => {
-            return <div class="zoo-layout-header">
-                <div class="zoo-layout-header-content">
-                    <a href="https://github.com/zoo-js" target="_blank">
-                        <img src={`https://avatars1.githubusercontent.com/u/70757173?s=60&v=4`} />
-                    </a>
-                    <a href="https://github.com/zoo-js/zoo-issue-helper" target="_blank">
-                        <h1>{props.title}</h1>
-                    </a>
-                    {slots.header?.()}
-                </div>
-            </div>
-        }
+	setup(props, { slots }) {
+		console.log(props.title)
+		const renderHeader = () => {
+			return <div class="zoo-layout-header">
+				<div class="zoo-layout-header-content">
+					<a href="https://github.com/zoo-js" target="_blank">
+						<img src={`https://avatars1.githubusercontent.com/u/70757173?s=60&v=4`} />
+					</a>
+					<a href={props.title.url} target="_blank">
+						<h1>{props.title.name}</h1>
+					</a>
+					{slots.header?.()}
+				</div>
+			</div>
+		}
 
-        return () => (
-            <div class="zoo-layout">
-                { renderHeader()}
-                <div class="zoo-layout-body">
-                    {slots.default?.()}
-                </div>
-            </div>
-        )
-    }
+		return () => (
+			<div class="zoo-layout">
+				{ renderHeader()}
+				<div class="zoo-layout-body">
+					{slots.default?.()}
+				</div>
+			</div>
+		)
+	}
 })
